e80b9d1651e12aa5d83ab7d37e1d7ecd774aead7
lizafalko <liza199797@mail.ru>
2018-02-08 13:25:54 +0300

    added actions

--- tic-tac-app/src/index.js
+++ tic-tac-app/src/index.js
@@ -1,44 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Button } from './button.js';
+import { createStore } from 'redux';
+/*
+ * типы действий
+ */
 
-class ShopSelection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 'Nike' };
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+/*
+ * другие константы
+ */
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('You favorite shop is ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Pick you favorite shop: </label>
-        <select className="selection" value={this.state.value} onChange={this.handleChange}>
-          <option value="FootBox">FootBox</option>
-          <option value="Nike">Nike</option>
-          <option value="Reebock">Reebock</option>
-          <option value="Pik">Pik</option>
-          <option value="Vans">Vans</option>
-        </select>
-        <input type="submit" value="Submit" />
-        </form>
-    );
-  }
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
 }
 
-// ReactDOM.render(
-//   <Button />,
-//   document.getElementById('root')
-// );
+/*
+ * генераторы действий
+ */
+
+export function addTodo(text) {
+  return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
